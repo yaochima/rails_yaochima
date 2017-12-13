a9161fbfb13ecd2858ee6e96cbcf2df5ae952646
Fabien Martin <fabien.martin87@gmail.com>
2017-12-13 15:02:47 +0800

    exclude restaurants

--- app/controllers/api/v1/shakes_controller.rb
+++ app/controllers/api/v1/shakes_controller.rb
@@ -6,17 +6,18 @@ class Api::V1::ShakesController < Api::V1::BaseController
     # @session_uuid = shake_params[:session_uuid]
     @lng = params[:lng].to_f
     @lat = params[:lat].to_f
-    p params[:lng]
-    p params[:lat]
+    # p params[:lng]
+    # p params[:lat]
+    @rest_exclusions = params[:rest_exclusions]
     @exclusions = params[:exclusions]
     @locked_category = params[:lockedcategory]
     @locked_price = params[:lockedprice].to_i
-    p "exclusions"
-    p @exclusions
-    p "locked_category"
-    p @locked_category
-    p "locked_price"
-    p @locked_price
+    # p "exclusions"
+    # p @exclusions
+    # p "locked_category"
+    # p @locked_category
+    # p "locked_price"
+    # p @locked_price
     #return a random restaurant
     @radius_km = 1
     return_random_restaurant
@@ -31,7 +32,7 @@ class Api::V1::ShakesController < Api::V1::BaseController
 
 
 
-    p @restaurant
+    # p @restaurant
 
     if @error_message.nil?
       render json: { status: "ok",
@@ -53,89 +54,38 @@ class Api::V1::ShakesController < Api::V1::BaseController
   # end
 
   def return_random_restaurant
-    p "RUN RETURN"
     return_near_restaurant_list
     return_rated_restaurant_list
     return_category_restaurant_list
     return_price_restaurant_list
+    return_different_restaurant_list
 
     @restaurants = @restaurants.near([@lat, @lng], @radius_km, :units => :km)
     if @error_message.nil?
       @restaurant = @restaurants.sample.id
     end
-
-
-    # return_restaurant_list
-
-    # #future radius extension
-    # # [ 1, 1.5, 2, 2,5, 3 ].each do |radius|
-    # #   if @restaurants.near([@lat, @lng], radius, :units => :km).sample.id
-    # #     return @restaurant = @restaurants.near([@lat, @lng], radius, :units => :km).sample.id
-    # #   end
-    # # end
-    # p "INITIATE"
-    # @restaurants.near([@lat, @lng], 1, :units => :km)
-    # p @restaurants.class
-    # if @restaurants.first.nil?
-
-    #   @error_message = "There are no more category available"
-    #   @exclusions = []
-    #   return_random_restaurant
-    # else
-    #   @restaurant = @restaurants.sample.id
-    # end
-    # # @restaurants = Restaurant.near([@lat, @lng], 10, :units => :km).sample.id
-    # # @restaurants = @restaurants.where(category: @locked_category)
   end
 
   def return_near_restaurant_list
     @restaurants = Restaurant.all
     @restaurants = @restaurants.near([@lat, @lng], @radius_km, :units => :km)
-    if @restaurants.first.nil?
-      if @radius_km == 3.0
-        @error_message = "没找到合适的结果…… 再摇一摇"
-      else
-        @radius_km += 0.5
-        return_random_restaurant
-      end
-    end
+    error_messages
   end
 
   def return_rated_restaurant_list
     @restaurants = @restaurants.where(rating: 3..6)
     @restaurants = @restaurants.where.not(price_per_person: 0)
-    if @restaurants.first.nil?
-      if @radius_km == 3.0
-        @error_message = "没找到合适的结果…… 再摇一摇"
-      else
-        @radius_km += 0.5
-        return_random_restaurant
-      end
-    end
+    error_messages
   end
 
   def return_category_restaurant_list
     if @locked_category
       @restaurants = @restaurants.where(category: @locked_category)
-      if @restaurants.first.nil?
-        if @radius_km == 3.0
-          @error_message = "没找到合适的结果…… 再摇一摇"
-        else
-          @radius_km  += 0.5
-          return_random_restaurant
-        end
-      end
+      error_messages
     elsif !@exclusions.nil?
       @exclusions.each do |food_category|
         @restaurants = @restaurants.where.not(category: food_category)
-        if @restaurants.first.nil?
-          if @radius_km == 3.0
-            @error_message = "没找到合适的结果…… 再摇一摇"
-          else
-            @radius_km  += 0.5
-            return_random_restaurant
-          end
-        end
+        error_messages
       end
     end
   end
@@ -145,40 +95,51 @@ class Api::V1::ShakesController < Api::V1::BaseController
       @max_price = @locked_price * 1.05
       @min_price = @locked_price * 0.75
       @restaurants = @restaurants.where(price_per_person: @min_price..@max_price)
-      if @restaurants.first.nil?
-        if @radius_km == 3.0
-          @error_message = "没找到合适的结果…… 再摇一摇"
-        else
-          @radius_km  += 0.5
-          return_random_restaurant
-        end
-      end
+      error_messages
     end
   end
 
-
-  def return_restaurant_list
-    @restaurants = Restaurant.all
-    if @locked_category
-        @restaurants = @restaurants.where(category: @locked_category)
-    elsif !@exclusions.nil?
-      @exclusions.each do |food_category|
-        @restaurants = @restaurants.where.not(category: food_category)
-      end
-    end
-    if @locked_price != 0
-      p @locked_price
-      calculate_price_range
-      @restaurants = @restaurants.where(price_per_person: @min_price..@max_price)
+  def return_different_restaurant_list
+    @rest_exclusions.each do |restaurant_id|
+      @restaurants.where.not(id: restaurant_id)
+      error_messages
     end
   end
 
-  def return_locked_category_restaurant_list
-    @restaurants = @restaurants.where(category: @locked_category)
+  def error_messages
     if @restaurants.first.nil?
-      @error_message = "没找到合适的结果…… 再摇一摇"
+      if @radius_km == 3.0
+        @error_message = "没找到合适的结果…… 再摇一摇"
+      else
+        @radius_km  += 0.5
+        return_random_restaurant
+      end
     end
   end
+
+
+  # def return_restaurant_list
+  #   @restaurants = Restaurant.all
+  #   if @locked_category
+  #       @restaurants = @restaurants.where(category: @locked_category)
+  #   elsif !@exclusions.nil?
+  #     @exclusions.each do |food_category|
+  #       @restaurants = @restaurants.where.not(category: food_category)
+  #     end
+  #   end
+  #   if @locked_price != 0
+  #     p @locked_price
+  #     calculate_price_range
+  #     @restaurants = @restaurants.where(price_per_person: @min_price..@max_price)
+  #   end
+  # end
+
+  # def return_locked_category_restaurant_list
+  #   @restaurants = @restaurants.where(category: @locked_category)
+  #   if @restaurants.first.nil?
+  #     @error_message = "没找到合适的结果…… 再摇一摇"
+  #   end
+  # end
 
 
   def calculate_price_range
